78f1c2d5dff9a8df5d3a2e93c2e4cc1d991f66dd
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-01-10 16:51:09 +0530

    even.C
    even or odd

---
+++ even.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+ {
+ int s;
+ printf("enter the value of s: ");
+ scanf("%d",&s);
+ if(s%2==0)
+ {
+ printf("the number is even");
+ }
+ else
+ {
+ printf("the number is odd");
+ }
+ return 0;
+ }
